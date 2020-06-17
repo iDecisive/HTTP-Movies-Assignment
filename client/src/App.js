b5fe5a8c14a1ea5b3fc50b2692ad2d1a7c887834
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-17 18:59:26 -0400

    Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; 
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
